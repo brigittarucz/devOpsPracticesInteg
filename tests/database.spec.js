7aabcc6a7ac699da5fc4bac9af0d83611ee072dc
HyunJi-Clara-Lee <hyun0036@stud.kea.dk>
2021-05-08 11:09:46 +0200

    Database connection test - works

--- tests/database.spec.js
+++ tests/database.spec.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const { expect } = require('chai');
-const { dbMaria } = require('../util/database-test');
+const dbMaria = require('../util/database-test');
 
 describe('Test database connection', () => {
     it('Should get user ID 1c1903d7...', (done) => {
         dbMaria.execute("SELECT * FROM users WHERE users.email = 'brigitt1a121@yahoo.com';")
             .then(res => {
                 var user = res[0][0];
-                console.log(user);
+                // console.log(user);
                 expect(user.id).to.equal('1c1903d7-e852-4578-8f39-69421a7849fa');
                 done();
             })
