99959cf5fbbdce670a9dcbe34ca11eef25cf3c08
HyunJi-Clara-Lee <hyun0036@stud.kea.dk>
2021-05-10 12:08:25 +0200

    database.specs - test added, after() moved

--- tests/database.spec.js
+++ tests/database.spec.js
@@ -9,12 +9,23 @@ describe('Test database connection', () => {
                 var user = res[0][0];
                 // console.log(user);
                 expect(user.id).to.equal('1c1903d7-e852-4578-8f39-69421a7849fa');
-
-                // Prevent test from hanging
-                // https://stackoverflow.com/questions/51046665/mocha-hangs-after-tests-have-finished
-                after(() => { dbMaria.end() })
                 done();
             })
             .catch(error => console.log(error))
     });
+
+    it('Should get event ID 3c1246a5...', (done) => {
+        dbMaria.execute("SELECT * FROM events WHERE events.title = 'Deep Dive Design Thinking Workshop';")
+            .then(res => {
+                var user = res[0][0];
+                // console.log(user);
+                expect(user.id).to.equal('3c1246a5-fee9-485d-adb3-24f029c528b4');
+                done();
+            })
+            .catch(error => console.log(error))
+    });
+
+    // Prevent test from hanging
+    // https://stackoverflow.com/questions/51046665/mocha-hangs-after-tests-have-finished
+    after(() => { dbMaria.end() })
 });
