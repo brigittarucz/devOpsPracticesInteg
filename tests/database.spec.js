f32902407dbbbf1187a9f2b433f0a90851763e77
HyunJi-Clara-Lee <hyun0036@stud.kea.dk>
2021-05-10 12:05:39 +0200

    database.specs modified to stop test from hanging

--- tests/database.spec.js
+++ tests/database.spec.js
@@ -9,6 +9,10 @@ describe('Test database connection', () => {
                 var user = res[0][0];
                 // console.log(user);
                 expect(user.id).to.equal('1c1903d7-e852-4578-8f39-69421a7849fa');
+
+                // Prevent test from hanging
+                // https://stackoverflow.com/questions/51046665/mocha-hangs-after-tests-have-finished
+                after(() => { dbMaria.end() })
                 done();
             })
             .catch(error => console.log(error))
