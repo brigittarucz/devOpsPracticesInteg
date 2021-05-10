4b91d8d83c3deadb53c5ada9cdd63385e340e3d0
HyunJi-Clara-Lee <hyun0036@stud.kea.dk>
2021-05-10 12:17:12 +0200

    Routes - chai-http installed

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -1,16 +1,35 @@
-// const app = require('../app');
-// const chai = require("chai");
-// const chaiHttp = require("chai-http");
-// chai.use(chaiHttp);
-// chai.should();
+const app = require('../app');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+chai.should();
 
-// describe("GET /", () => {
-//     it('Should get 200', (done) => {
-//     chai.request(app)
-//         .get('/')
-//         .end((err, res) => {
-//             res.should.have.status(200);
-//             done();
-//         })
-//     }
-// )})
+describe("GET /authenticate", () => {
+    it('Should get 200', (done) => {
+        chai.request(app)
+            .get('/authenticate')
+            .end((err, res) => {
+                console.log(res)
+                res.should.have.status(200);
+                done();
+            })
+    })
+})
+
+// describe("GET", () => {
+//     it('should send parameters to : /path POST', function (done) {
+//         chai
+//             .request(host)
+//             .post(path)
+//             // .field('myparam' , 'test')
+//             .set('content-type', 'application/x-www-form-urlencoded')
+//             .send({ myparam: 'test' })
+//             .end(function (error, response, body) {
+//                 if (error) {
+//                     done(error);
+//                 } else {
+//                     done();
+//                 }
+//             });
+//     });
+// })
