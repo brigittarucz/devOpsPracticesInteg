c989049980436e31bb9567f9ad21ec2ff0f9fcd6
HyunJi-Clara-Lee <hyun0036@stud.kea.dk>
2021-05-10 11:31:19 +0200

    Eslint - public/js/sort-dashboard.js modified

--- public/js/sort-dashboard.js
+++ public/js/sort-dashboard.js
@@ -4,17 +4,17 @@ var relevanceAscending = 1;
 function sortByRelevance() {
     var aEvents = document.querySelector(".main__home-events_container");
 
-    if(relevanceAscending) {
+    if (relevanceAscending) {
         // Ascending
 
-        var newOrder = Array.from(aEvents.children).sort(function(a,b) {
+        var newOrder = Array.from(aEvents.children).sort(function (a, b) {
             return getRelevance(b) - getRelevance(a);
         });
         event.target.textContent = 'Relevance: Lowest to Highest';
     } else {
         // Descending
-        
-        var newOrder = Array.from(aEvents.children).sort(function(a,b) {
+
+        var newOrder = Array.from(aEvents.children).sort(function (a, b) {
             return getRelevance(a) - getRelevance(b);
         });
         event.target.textContent = 'Relevance: Highest to Lowest';
@@ -37,17 +37,17 @@ function sortByDate() {
 
     var aEvents = document.querySelector(".main__home-events_container");
 
-    if(dateAscending) {
+    if (dateAscending) {
         // Ascending
 
-        var newOrder = Array.from(aEvents.children).sort(function(a,b) {
+        var newOrder = Array.from(aEvents.children).sort(function (a, b) {
             return getRelevance(b) - getRelevance(a);
         });
         event.target.textContent = 'Date: Latest to Closest';
     } else {
         // Descending
-        
-        var newOrder = Array.from(aEvents.children).sort(function(a,b) {
+
+        var newOrder = Array.from(aEvents.children).sort(function (a, b) {
             return getRelevance(a) - getRelevance(b);
         });
         event.target.textContent = 'Date: Closest to Latest';
@@ -60,6 +60,6 @@ function sortByDate() {
     dateAscending = !dateAscending;
 }
 
-function getRelevance(queryElement) {
-    return queryElement.getAttribute("data-timestamp");
-}
+// function getRelevance(queryElement) {
+//     return queryElement.getAttribute("data-timestamp");
+// }
