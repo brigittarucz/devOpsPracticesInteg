e61571d1ca657ad170bc8ab6909d81946598736d
“HyunJi-Clara-Lee” <hyun0036@stud.kea.dk>
2021-06-28 03:33:39 +0200

    Testing 'host' of mysql for db_test

--- test/db_test.js
+++ test/db_test.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createConnection({
-    host: 'localhost',
+    host: 'mariadb_dummy',
     user: 'root',
     database: 'techevents_users',
     password: 'test',
     port: 3308
 });
 
-pool.connect(function(err) {
-    if(err !== null) {
-        if(err.errno === 'ENOTFOUND') {
+pool.connect(function (err) {
+    if (err !== null) {
+        if (err.errno === 'ENOTFOUND') {
             pool.host = 'localhost';
         }
     }
